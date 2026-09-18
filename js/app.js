593808530ed8da8ab72af5a92dce3cabb8163a43
Baptiste <Baptistebrs13@gmail.com>
2026-09-18 16:57:56 +0200

    ajustement table et (retour à la collection) en cours

--- js/app.js
+++ js/app.js
@@ -39,8 +39,8 @@ fetch("https://pokeapi.co/api/v2/pokemon/"+ id)
     document.querySelector('.pokemon-image').src = pokemon.sprites.other['official-artwork'].front_default;
     document.querySelector('.pokemon-image').alt = pokemon.name;
     document.querySelector('.type').textContent = traductionTypes[pokemon.types[0].type.name];
-    document.querySelector('.taille').textContent = pokemon.height + "m";
-    document.querySelector('.poids').textContent = pokemon.weight +"kg";
+    document.querySelector('.taille').textContent = pokemon.height/10 + " m";
+    document.querySelector('.poids').textContent = pokemon.weight/10 +" kg";
     document.querySelector('.attaque').textContent = pokemon.stats[1].base_stat;
     document.querySelector('.défense').textContent = pokemon.stats[2].base_stat;
     document.querySelector('.pv').textContent = pokemon.stats[0].base_stat;
@@ -54,49 +54,3 @@ if (window.pokedexFavorites) {
     window.pokedexFavorites.displayFavorites();
 }
 
-
-
-
-
-
-
-// async loadPokemon() {
-
-//         const response = await fetch(
-//             "https://pokeapi.co/api/v2/pokemon?limit=151"
-//         );
-
-//         if (!response.ok) {
-//             throw new Error(
-//                 "Erreur lors du chargement de la liste."
-//             );
-//         }
-
-//         const data = await response.json();
-
-
-//         /*
-//          * La première requête donne les 151 Pokémon.
-//          * Ensuite on récupère les détails de chacun.
-//          */
-
-//         const requests = data.results.map(
-//             async (pokemon) => {
-
-//                 const response =
-//                     await fetch(pokemon.url);
-
-//                 if (!response.ok) {
-//                     throw new Error(
-//                         `Erreur pour ${pokemon.name}`
-//                     );
-//                 }
-
-//                 return await response.json();
-//             }
-//         );
-
-
-//         this.pokemonList =
-//             await Promise.all(requests);
-//     }
